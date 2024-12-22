5901f49373d68c0f5a6a6bfc5a3ec371792cf1ab
Sai Charan <138667126+M-SaiCharan@users.noreply.github.com>
2024-12-22 10:06:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ data_path = r"Iris - all-numbers.csv"
 df = pd.read_csv(data_path)
 
 def menu():
-    print("**Pandas**")
-    print("\n\t 1. Math Operations \n\t 2. Graph \n\t 3. print \n\t 4. Exit\n")
+    print("\nPandas-Application")
+    print("\033[38;5;250m\n\t 1. Math Operations \n\t 2. Graph \n\t 3. print \n\t 4. Exit\n\033[0m")
     choice = input("Enter your choice: ")
     while choice != "4":
         if choice == "1":
@@ -23,48 +23,65 @@ def menu():
 
 
 def printdf():
-    print(df)
-    menu()
+    print("\nPrint Options: \n")
+    print("\033[38;5;250m\t 1. Entire DataFrame \n\t 2. First 5 rows \n\t 3. Last 5 rows \n\t 4. Shape of the DataFrame \n\t 5. Columns of the DataFrame \n\t 6. Prev Menu\n\033[0m")
+    choice = int(input("Enter your choice: "))
+    print("\n")
+    if choice == 1:
+        print(df)
+    elif choice == 2:
+        print(df.head(5))
+    elif choice == 3:
+        print(df.tail(5))
+    elif choice == 4:
+        print(df.shape)
+    elif choice == 5:
+        print(df.columns)
+    elif choice == 6:
+        menu()
+    else:
+        print("Invalid choice\n")
+        printdf()
 
 def math_operations():
-    print("\n**MATH OPERATIONS**")
-    print('\n\t 1.Mean\n\t 2.Median\n\t 3.Mode \n\t 4.Sum\n\t 5.Prev Menu')
+    print("\nMATH OPERATIONS")
+    print('\033[38;5;250m\n\t 1.Mean\n\t 2.Median\n\t 3.Mode \n\t 4.Sum\n\t 5.Prev Menu\033[0m')
     choice = int(input('\nEnter your choice: '))
 
     if choice == 1:
         opt_col = input(str("Enter the col: "))
         if opt_col in df.columns:
             mean = df[opt_col].mean()
-            print(f"The mean of the {opt_col}: {mean}")
+            print(f"\nThe mean of the {opt_col}: \033[32m{mean}\033[0m")
         else:
-            print(f"The column {opt_col} does not exist in the dataframe")
+            print(f"\nThe column {opt_col} does not exist in the dataframe")
         math_operations()
     
     elif choice == 2:
         opt_col = input(str("Enter the col: "))
         if opt_col in df.columns:
             median = df[opt_col].median()
-            print(f"The median of the {opt_col}: {median}")
+            print(f"\nThe median of the {opt_col}: \033[32m{median}\033[0m")
         else:
-            print(f"The column {opt_col} does not exist in the dataframe")
+            print(f"\nThe column {opt_col} does not exist in the dataframe")
         math_operations()
 
     elif choice == 3:
         opt_col = input(str("Enter the col: "))
         if opt_col in df.columns:
             mode = df[opt_col].mode()
-            print(f"The mode of the {opt_col}: {mode}")
+            print(f"\nThe mode of the {opt_col}: \033[32m{mode}\033[0m")
         else:
-            print(f"The column {opt_col} does not exist in the dataframe")
+            print(f"\nThe column {opt_col} does not exist in the dataframe")
         math_operations()
 
     elif choice == 4:
         opt_col = input(str("Enter the col: "))
         if opt_col in df.columns:
             col_sum = df[opt_col].sum()
-            print(f"The sum of the {opt_col}: {col_sum}")
+            print(f"\nThe sum of the {opt_col}: \033[32m{col_sum}\033[0m")
         else:
-            print(f"The column {opt_col} does not exist in the dataframe")
+            print(f"\nThe column {opt_col} does not exist in the dataframe")
         math_operations()
 
     elif choice == 5:
@@ -76,8 +93,9 @@ def math_operations():
 
 
 def graph():
+    print("\nGRAPH OPERATIONS")
     print("\nEnter the graph type:\n")
-    print("\t 1. Line Graph \n\t 2. Scatter Plot \n\t 3. Bar Plot \n\t 4. Histogram \n\t 5. Pie Chart \n\t 6. Heat Map \n\t 7. Prev Menu\n")
+    print("\033[38;5;250m\t 1. Line Graph \n\t 2. Scatter Plot \n\t 3. Bar Plot \n\t 4. Histogram \n\t 5. Pie Chart \n\t 6. Heat Map \n\t 7. Prev Menu\n\033[0m")
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
@@ -154,4 +172,5 @@ def validate_columns(columns):
             return False
     return True
 
-menu()
+if __name__ == "__main__":
+    menu()
